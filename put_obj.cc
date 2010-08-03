9592e169ce3428abbb6dcf7b392092fe75a0fe7d
syuu <syuu@dokukino.com>
2010-08-03 22:42:41 +0900

    hoge

--- put_obj.cc
+++ put_obj.cc
@@ -1,46 +1,47 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include <rados/librados.h>
+#include <string>
+
 #include <rados/librados.hpp>
 using namespace librados;
 
-#include <string>
-using namespace std;
-
-Rados *rados = NULL;
+static void usage(void)
+{
+  std::cout << "put_obj [bucket] [oid] [file_name]" << std::endl;
+}
 
 int main(int argc, const char **argv)
 {
+  if (argc < 4)
+    usage();
+
   int r;
   std::string bucket(argv[1]);
   std::string oid(argv[2]);
   std::string file_name(argv[3]);
-  rados_pool_t pool;
+  pool_t pool;
   bufferlist bl;
+  Rados rados;
 
-  rados = new Rados();
-  if (!rados)
-    return -ENOMEM;
-
-  r = rados->initialize(0, NULL);
+  r = rados.initialize(0, NULL);
   if (r < 0)
-    goto err;
+    return r;
 
-  r = rados->open_pool(bucket.c_str(), &pool);
+
+  r = rados.open_pool(bucket.c_str(), &pool);
   if (r < 0)
-    goto err;
+    return r;
 
   r = bl.read_file(file_name.c_str());
   if (r < 0)
     goto err;
 
-  r = rados->write_full(pool, oid, bl);
+  r = rados.write_full(pool, oid, bl);
   if (r < 0)
     goto err;
 
  err:
-  rados->close_pool(pool);
-  delete rados;
+  rados.close_pool(pool);
   return r;
 }
